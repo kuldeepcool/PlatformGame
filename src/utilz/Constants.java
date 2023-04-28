0a60e4a56ed0b488b74d1fc0e24730f41f2576c2
kuldeepcool <cool.kuldeep344@gmail.com>
2023-04-28 18:20:31 +0530

    Added more CONSTANTS required for UI buttons

--- src/utilz/Constants.java
+++ src/utilz/Constants.java
@@ -5,7 +5,7 @@ import main.Game;
 public class Constants {
 
 	public static class UI {
-		public static class Buttons {
+		public static class MenuButtons {
 			public static final int B_DEFAULT_WIDTH = 140;
 			public static final int B_DEFAULT_HEIGHT = 56;
 			public static final int B_WIDTH = (int) (B_DEFAULT_WIDTH * Game.SCALE);
@@ -16,6 +16,20 @@ public class Constants {
 
 			public static final int SOUND_SIZE_DEFAULT = 42;
 			public static final int SOUND_SIZE = (int) (SOUND_SIZE_DEFAULT * Game.SCALE);
+		}
+
+		public static class URMButtons {
+			public static final int URM_SIZE_DEFAULT = 56;
+			public static final int URM_SIZE = (int) (URM_SIZE_DEFAULT * Game.SCALE);
+		}
+
+		public static class VolumeButtons {
+			public static final int VOLUME_DEFAULT_WIDTH = 28;
+			public static final int VOLUME_DEFAULT_HEIGHT = 44;
+			public static final int SLIDER_DEFAULT_WIDTH = 215;
+			public static final int VOLUME_WIDTH = (int) (VOLUME_DEFAULT_WIDTH * Game.SCALE);
+			public static final int VOLUME_HEIGHT = (int) (VOLUME_DEFAULT_HEIGHT * Game.SCALE);
+			public static final int SLIDER_WIDTH = (int) (SLIDER_DEFAULT_WIDTH * Game.SCALE);
 
 		}
 	}
