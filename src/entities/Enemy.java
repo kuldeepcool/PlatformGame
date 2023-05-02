0a2417093091596d26101c41003f5da6b2615e33
kuldeepcool <cool.kuldeep344@gmail.com>
2023-05-02 15:51:26 +0530

    Added classes to handle different types of enemies

---
+++ src/entities/Enemy.java
@@ -0,0 +1,45 @@
+package entities;
+
+import static utilz.Constants.EnemyConstants.*;
+
+public abstract class Enemy extends Entity {
+
+	private int enemyState;
+	private int enemyType;
+	private int animationIndex;
+	private int animationTick;
+	private int animationSpeed = 25;
+
+	public Enemy(float x, float y, int width, int height, int enemyType) {
+
+		super(x, y, width, height);
+		this.enemyType = enemyType;
+		initHitBox(x, y, width, height);
+	}
+
+	private void updateAnimationTick() {
+
+		animationTick++;
+		if (animationTick >= animationSpeed) {
+			animationTick = 0;
+			animationIndex++;
+			if (animationIndex >= GetSpriteAmount(enemyType, enemyState)) {
+				animationIndex = 0;
+			}
+		}
+	}
+
+	public void update() {
+
+		updateAnimationTick();
+
+	}
+
+	public int getAnimationIndex() {
+		return animationIndex;
+	}
+
+	public int getEnemyState() {
+		return enemyState;
+	}
+}
---
+++ src/entities/EnemyManager.java
@@ -0,0 +1,55 @@
+package entities;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+import gamestates.Playing;
+import utilz.LoadSave;
+import static utilz.Constants.EnemyConstants.*;
+
+public class EnemyManager {
+
+	private Playing playing;
+
+	private BufferedImage[][] crabbyArr;
+	private ArrayList<Crabby> crabbies = new ArrayList<>();
+
+	public EnemyManager(Playing playing) {
+		this.playing = playing;
+		loadEnemyImages();
+		addEnemies();
+	}
+
+	private void addEnemies() {
+		crabbies = LoadSave.getCrabs();
+		System.out.println("size of crabs " + crabbies.size());
+	}
+
+	public void update() {
+		for (Crabby c : crabbies)
+			c.update();
+	}
+
+	public void draw(Graphics g, int xLevelOffset) {
+
+		drawCrabs(g, xLevelOffset);
+
+	}
+
+	private void drawCrabs(Graphics g, int xLevelOffset) {
+		for (Crabby c : crabbies)
+			g.drawImage(crabbyArr[c.getEnemyState()][c.getAnimationIndex()], (int) (c.getHitBox().x - xLevelOffset),
+					(int) c.getHitBox().y, CRABBY_WIDTH, CRABBY_HEIGHT, null);
+	}
+
+	private void loadEnemyImages() {
+		crabbyArr = new BufferedImage[5][9];
+		BufferedImage temp = LoadSave.GetSpriteAtlas(LoadSave.CRABBY_SPRITE);
+		for (int j = 0; j < crabbyArr.length; ++j)
+			for (int i = 0; i < crabbyArr[j].length; ++i)
+				crabbyArr[j][i] = temp.getSubimage(i * CRABBY_WIDTH_DEFAULT, j * CRABBY_HEIGHT_DEFAULT,
+						CRABBY_WIDTH_DEFAULT, CRABBY_HEIGHT_DEFAULT);
+	}
+
+}
